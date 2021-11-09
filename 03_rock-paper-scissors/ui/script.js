815583a97c8277b76cf89b7fff941fe6faac0030
John Antoine G <john.antoine.g@gmail.com>
2021-11-10 01:08:02 +0200

    game complete - broken animation timings

--- 03_rock-paper-scissors/ui/script.js
+++ 03_rock-paper-scissors/ui/script.js
@@ -1,20 +1,32 @@
 // DOM Selectors
 
-const btnOpenRules = document.querySelector('.rules-button');
-const btnCloseRules = document.querySelector('.modal-close');
-const btnChoices = document.querySelectorAll('.choice-wrapper');
+const btnOpenRules = document.querySelector(".rules-button");
+const btnCloseRules = document.querySelector(".modal-close");
+const btnChoices = document.querySelectorAll(".choice-wrapper");
 const btnRock = document.querySelector('.choice-wrapper[data-choice="rock"]');
 const btnPaper = document.querySelector('.choice-wrapper[data-choice="paper"]');
-const btnScissors = document.querySelector('.choice-wrapper[data-choice="scissors"]');
-const modalRulesWrapper = document.querySelector('.modal-wrapper');
-const modalRulesContent = document.querySelector('.modal-content');
-const bgChoices = document.querySelector('.game-selection-bg');
-const containerGame = document.querySelector('.game-container');
+const btnScissors = document.querySelector(
+  '.choice-wrapper[data-choice="scissors"]'
+);
+const modalRulesWrapper = document.querySelector(".modal-wrapper");
+const modalRulesContent = document.querySelector(".modal-content");
+const bgChoices = document.querySelector(".game-selection-bg");
+const containerGame = document.querySelector(".game-container");
+const scoreCounter = document.querySelector(".score-points");
+const containerResult = document.querySelector(".result-prompt");
+const btnResult = document.querySelector(".result-prompt button");
+const roundResult = document.querySelector(".result-prompt h3");
+
+// Set score in local storage
+
+if (!localStorage.getItem("score")) localStorage.setItem("score", 0);
+let score = localStorage.getItem("score");
+scoreCounter.textContent = score;
 
 // CSS Transition Injection on page load
 
-document.addEventListener('readystatechange', () => {
-  if (document.readyState === 'complete') {
+document.addEventListener("readystatechange", () => {
+  if (document.readyState === "complete") {
     const styleSheet = document.styleSheets[0];
 
     const gameSelectionBgVisible = `
@@ -38,16 +50,16 @@ document.addEventListener('readystatechange', () => {
       }
     `;
 
-    const choiceHouseDelay = `
-      .choice-1.house ,
-      .choice-2.house ,
-      .choice-3.house {
-        transition: transform ease-in-out var(--animation-duration)
-                    var(--animation-duration),
-                    opacity ease-in-out var(--animation-duration)
-                    var(--animation-duration);
-      }
-    `;
+    // const choiceHouseDelay = `
+    //   .choice-1.house ,
+    //   .choice-2.house ,
+    //   .choice-3.house {
+    //     transition: transform ease-in-out var(--animation-duration)
+    //                 var(--animation-duration),
+    //                 opacity ease-in-out var(--animation-duration)
+    //                 var(--animation-duration);
+    //   }
+    // `;
 
     const bgTop = `
       .bg-top {
@@ -73,16 +85,25 @@ document.addEventListener('readystatechange', () => {
         transition: opacity ease-in-out var(--animation-duration),
                     visibility;
       }
-    `
+    `;
+
+    const transitionResultPrompt = `
+      .result-prompt {
+        transition: opacity ease-in-out var(--animation-duration),
+                    transform ease-in-out var(--animation-duration),
+                    visibility var(--animation-duration);
+      }
+    `;
 
     styleSheet.insertRule(choiceDefault, styleSheet.rules.length);
     styleSheet.insertRule(gameSelectionBgVisible, styleSheet.rules.length);
     styleSheet.insertRule(choiceWrapperDefault, styleSheet.rules.length);
-    styleSheet.insertRule(choiceHouseDelay, styleSheet.rules.length);
+    // styleSheet.insertRule(choiceHouseDelay, styleSheet.rules.length);
     styleSheet.insertRule(bgTop, styleSheet.rules.length);
     styleSheet.insertRule(bgShadow, styleSheet.rules.length);
     styleSheet.insertRule(transitionModalOpen, styleSheet.rules.length);
     styleSheet.insertRule(transitionModalClosed, styleSheet.rules.length);
+    styleSheet.insertRule(transitionResultPrompt, styleSheet.rules.length);
 
     showInitialState();
   }
@@ -90,110 +111,156 @@ document.addEventListener('readystatechange', () => {
 
 // Modal Event Hnadling
 
-btnOpenRules.addEventListener('click', () => modalRulesWrapper.classList.add('modal-show'));
-btnCloseRules.addEventListener('click', () => modalRulesWrapper.classList.remove('modal-show'));
-modalRulesWrapper.addEventListener('click', () => {
-  modalRulesWrapper.classList.remove('modal-show');
+btnOpenRules.addEventListener("click", () =>
+  modalRulesWrapper.classList.add("modal-show")
+);
+btnCloseRules.addEventListener("click", () =>
+  modalRulesWrapper.classList.remove("modal-show")
+);
+modalRulesWrapper.addEventListener("click", () => {
+  modalRulesWrapper.classList.remove("modal-show");
 });
-modalRulesContent.addEventListener('click', e => {
+modalRulesContent.addEventListener("click", (e) => {
   e.stopPropagation();
 });
 
 // Control Game State Animation
 
 const hideInitialBg = () => {
-  bgChoices.classList.add('hidden');
+  bgChoices.classList.add("hidden");
 };
 
 const showInitialBg = () => {
-  bgChoices.classList.remove('hidden');
+  bgChoices.classList.remove("hidden");
 };
 
 const hideInitialChoices = () => {
-  btnChoices.forEach(choice => {
-    choice.classList.remove('initial');
-    choice.classList.add('hidden');
+  btnChoices.forEach((choice) => {
+    choice.classList.remove("initial");
+    choice.classList.add("hidden");
   });
 };
 
 const showInitialChoices = () => {
-  btnChoices.forEach(choice => {
-    choice.classList.remove('hidden');
-    choice.classList.add('initial');
+  btnChoices.forEach((choice) => {
+    choice.classList.remove("hidden");
+    choice.classList.add("initial");
   });
 };
 
-btnChoices.forEach(choice => {
-  choice.addEventListener('transitionend', showInitialBg);
+btnChoices.forEach((choice) => {
+  choice.addEventListener("transitionend", showInitialBg);
 });
 
 const hideInitialState = () => {
-  btnChoices.forEach(choice => {
-    choice.removeEventListener('transitionend', showInitialBg);
-    choice.addEventListener('transitionend', markAnimationEnd);
+  btnChoices.forEach((choice) => {
+    choice.addEventListener(
+      "transitionend",
+      () => {
+        markAnimationEnd("choiceEnd");
+      },
+      { once: true }
+    );
   });
-  bgChoices.addEventListener('transitionend', hideInitialChoices);
+  bgChoices.addEventListener("transitionend", hideInitialChoices, {
+    once: true,
+  });
   hideInitialBg();
-}
+};
 
 const showInitialState = () => {
-  bgChoices.removeEventListener('transitionend', hideInitialChoices);
-  btnChoices.forEach(choice => {
-    choice.addEventListener('transitionend', showInitialBg);
-    choice.removeEventListener('transitionend', markAnimationEnd);
+  btnChoices.forEach((choice) => {
+    choice.addEventListener("transitionend", showInitialBg, { once: true });
   });
   showInitialChoices();
-}
+};
 
 // Choice Event Hnadling
 
-btnChoices.forEach(choice => {
-  choice.addEventListener('click', () => {
-
+btnChoices.forEach((choice) => {
+  choice.addEventListener("click", () => {
     const choiceHouse = computerPlay();
     const choicePlayer = choice.dataset.choice.toLowerCase();
 
     const tagPlayer = document
-          .querySelector(`.choice-wrapper[data-choice="${choicePlayer}"]`)
-          .cloneNode(true);
+      .querySelector(`.choice-wrapper[data-choice="${choicePlayer}"]`)
+      .cloneNode(true);
     const tagHouse = document
-          .querySelector(`.choice-wrapper[data-choice="${choiceHouse}"]`)
-          .cloneNode(true);
+      .querySelector(`.choice-wrapper[data-choice="${choiceHouse}"]`)
+      .cloneNode(true);
 
-    tagHouse.classList.add('hidden');
-    tagPlayer.classList.add('hidden');
+    tagHouse.classList.add("hidden");
+    tagPlayer.classList.add("hidden");
 
     hideInitialState();
+
+    tagPlayer.addEventListener(
+      "transitionend",
+      () => {
+        markAnimationEnd("tagPlayerTransition");
+      },
+      { once: true }
+    );
+
+    tagHouse.addEventListener(
+      "transitionend",
+      () => {
+        markAnimationEnd("tagHouseTransition");
+      },
+      { once: true }
+    );
+
+    window.addEventListener("animation_end", (e) => {
+      switch (e.detail.name) {
+        case "choiceEnd":
+          tagPlayer.classList.remove("hidden", "initial");
+          tagPlayer.classList.add("chosen");
+          break;
+        case "tagPlayerTransition":
+          tagHouse.classList.remove("hidden", "initial");
+          tagHouse.classList.add("house");
+          break;
+        case "tagHouseTransition":
+          tagHouse.classList.add("round-prompt");
+          tagPlayer.classList.add("round-prompt");
+          tagHouse.addEventListener(
+            "transitionend",
+            () => {
+              markAnimationEnd("tagHouseEnd");
+            },
+            { once: true }
+          );
+          break;
+        case "tagHouseEnd":
+          containerResult.classList.remove("hidden");
+          break;
+      }
+    });
 
     containerGame.appendChild(tagHouse);
     containerGame.appendChild(tagPlayer);
 
-    window.addEventListener('animation_end', () => {
-      tagHouse.classList.remove('hidden', 'initial');
-      tagHouse.classList.add('house');
-      tagPlayer.classList.remove('hidden', 'initial');
-      tagPlayer.classList.add('chosen');
-    });
-
     if (choiceHouse === choicePlayer) {
-      console.log(`It's a Draw! Player Chose ${choicePlayer}, Computer Chose ${choiceHouse}`);
+      roundResult.textContent = "DRAW";
     } else if (playRound(choicePlayer, choiceHouse)) {
-      console.log(`Player Win! Player Chose ${choicePlayer}, Computer Chose ${choiceHouse}`);
+      roundResult.textContent = "YOU WIN";
+      scoreCounter.textContent = ++score;
+      localStorage.setItem("score", score);
     } else {
-      console.log(`Player Lose! Player Chose ${choicePlayer}, Computer Chose ${choiceHouse}`);
+      roundResult.textContent = "YOU LOSE";
+      scoreCounter.textContent = --score;
+      localStorage.setItem("score", score);
     }
-
   });
 });
 
+// Restart Round
 
+btnResult.addEventListener("click", () => {
+  tempRevert();
+});
 
-
-
-
-
-
-
+// Game Logic
 
 function computerPlay() {
   let choice = Math.floor(Math.random() * 3 + 1);
@@ -231,80 +298,39 @@ function playRound(playerSelection, computerSelection) {
 
 // Custom Event Handler for animationEnd
 
-const animationEnd = new CustomEvent('animation_end');
-
-const markAnimationEnd = () => {
-  window.dispatchEvent(animationEnd);
-}
-
-// function capitalize(word) {
-//   return word.replace(/^[a-z]/, (c) => c.toUpperCase());
-// }
-
-// function game() {
-//   let round = 0;
-//   let playerScore = 0;
-
-//   while (1) {
-//     const computerSelection = computerPlay();
-//     let playerSelection = prompt("Choose Your Weapon! Rock | Paper | Scissors");
-//     let playerWin = false;
-
-//     while (
-//       playerSelection.toLowerCase() !== "rock" &&
-//       playerSelection.toLowerCase() !== "paper" &&
-//       playerSelection.toLowerCase() !== "scissors"
-//     ) {
-//       playerSelection = prompt(
-//         "Invalid Choice!\nChoose Your Weapon! Rock | Paper | Scissors"
-//       );
-//     }
-
-//     playerSelection = capitalize(playerSelection.toLowerCase());
-
-//     playerWin = playRound(playerSelection, computerSelection);
-
-//     if (playerWin) {
-//       console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
-//       alert(`You Win! ${playerSelection} beats ${computerSelection}`);
-//       playerScore++;
-//     } else if (playerSelection === computerSelection) {
-//       console.log(`Draw! Both players chose ${playerSelection}`);
-//       alert(`Draw! Both players chose ${playerSelection}`);
-//     } else {
-//       console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-//       alert(`You Lose! ${computerSelection} beats ${playerSelection}`);
-//     }
-
-//     round++;
-//   }
-
-//   if (playerScore > 2) {
-//     console.log(`You Win The Game!!\nYou Won ${playerScore} rounds out of 5!`);
-//     alert(`You Win The Game!!\nYou Won ${playerScore} rounds out of 5!`);
-//   } else {
-//     console.log(
-//       `You Lose The Game!!\nYou Lost ${5 - playerScore} rounds out of 5!`
-//     );
-//     alert(`You Lose The Game!!\nYou Lost ${5 - playerScore} rounds out of 5!`);
-//   }
-// }
-
-// game();
+const markAnimationEnd = (name) => {
+  const event = new CustomEvent("animation_end", {
+    detail: {
+      name: name,
+    },
+  });
+  window.dispatchEvent(event);
+};
 
 // TEMP FUNCTIONS
 
-const tempRemoveInitial = () => {
-  btnChoices.forEach(choice => {
-    choice.classList.remove('initial');
+const tempRevert = () => {
+  const tagHouse = document.querySelectorAll(".house");
+  const tagPlayer = document.querySelectorAll(".chosen");
+  const tags = [tagHouse, tagPlayer];
+  tags.forEach((tagList) => {
+    tagList.forEach((tag) => {
+      tag.addEventListener(
+        "transitionend",
+        () => {
+          tag.parentNode.removeChild(tag);
+        },
+        { once: true }
+      );
+      tag.classList.remove("round-prompt", "house");
+      tag.classList.add("hidden");
+    });
   });
-};
-
-const tempRemoveGame = () => {
-  document.querySelectorAll('.chosen').forEach(one => {
-    one.parentElement.removeChild(one);
+  containerResult.addEventListener("transitionend", () => {
+    showInitialState();
+    containerResult.removeEventListener("transitionend", () => {
+      showInitialState();
+    });
   });
-  document.querySelectorAll('.house').forEach(two => {
-    two.parentElement.removeChild(two);
-  });
+  containerResult.classList.add("hidden");
 };
