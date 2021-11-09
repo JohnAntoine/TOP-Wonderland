88f760c49f1a1741ca1684ea79c66340b1fdbccc
John Antoine <john.antoine.g@gmail.com>
2021-11-09 19:29:34 +0200

    Almost done - terrible commit message

--- 03_rock-paper-scissors/ui/README.md
+++ 03_rock-paper-scissors/ui/README.md
@@ -1,30 +1,14 @@
-# PROJECT 03: ROCK PAPER SCISSORS
+# PROJECT 03: ROCK PAPER SCISSORS - GUI
 
-Creating (Rock - Paper - Scissors) as a Console game
+Creating (Rock - Paper - Scissors) as a Desktop Graphical User Interface
 
-## Steps
+## Description
 
-* Start a new Git repo for your project.
-* Create a blank HTML document with a script tag (Hint: it is best practice to link an external .js file). This game is going to be played completely from the console, so don’t worry about putting anything else in there.
-* Your game is going to play against the computer, so begin with a function called computerPlay that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play. Tip: use the console to make sure this is returning the expected output before moving to the next step!
-* Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-* Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation). \
+Combining [The Odin Project](https://theodinproject.com/) and [Front End Mentor](https://www.frontendmentor.io/), the [**Rock Paper Scissors**](https://www.frontendmentor.io/challenges/rock-paper-scissors-game-pTgwgvgH) challenge by [Front End Mentor](https://www.frontendmentor.io/) was used to transform the Rock Paper Scissors [JavaScript project](https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/rock-paper-scissors), part of [The Odin Project](https://theodinproject.com/)'s [Foundations](https://www.theodinproject.com/paths/foundations/courses/foundations) course, from a **Console** based game to a **Graphical User Interface** game.
 
-**Important note: you want to return the results of this function call, not console.log() them. To test this function console.log the results:**
+## Workflow
 
-``` javascript
-function playRound(playerSelection, computerSelection) {
-  // your code here!
-}
-
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
-```
-
-* Write a NEW function called `game()`. Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
-  1. You have not officially learned how to “loop” over code to repeat function calls… if you already know about loops from somewhere else (or if you feel like doing some more learning) feel free to use them. If not, don’t worry! Just call your playRound function 5 times in a row. Loops are covered in the next lesson.
-  1. At this point you should be using `console.log()` to display the results of each round and the winner at the end.
-  1. Use `prompt()` to get input from the user. [Read the docs here if you need to.](https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt)
-  1. Feel free to re-work your previous functions if you need to. Specifically, you might want to change the return value to something more useful.
-  1. Feel free to create more “helper” functions if you think it would be useful.
+- The starter FronEndMentor `index.html` was used as a template to kickstart development.
+- Most of JavaScript code was morphed to control HTML elements instead of relying on user text input.
+- The project CSS was created from scratch to closely emulate the FrontEndMentor reference.
+- All the graphics were provided as part of the FrontEndMentor challenge files.
--- 03_rock-paper-scissors/ui/script.js
+++ 03_rock-paper-scissors/ui/script.js
@@ -1,90 +1,310 @@
+// DOM Selectors
+
+const btnOpenRules = document.querySelector('.rules-button');
+const btnCloseRules = document.querySelector('.modal-close');
+const btnChoices = document.querySelectorAll('.choice-wrapper');
+const btnRock = document.querySelector('.choice-wrapper[data-choice="rock"]');
+const btnPaper = document.querySelector('.choice-wrapper[data-choice="paper"]');
+const btnScissors = document.querySelector('.choice-wrapper[data-choice="scissors"]');
+const modalRulesWrapper = document.querySelector('.modal-wrapper');
+const modalRulesContent = document.querySelector('.modal-content');
+const bgChoices = document.querySelector('.game-selection-bg');
+const containerGame = document.querySelector('.game-container');
+
+// CSS Transition Injection on page load
+
+document.addEventListener('readystatechange', () => {
+  if (document.readyState === 'complete') {
+    const styleSheet = document.styleSheets[0];
+
+    const gameSelectionBgVisible = `
+      .game-selection-bg {
+        transition: ease-in-out calc(var(--animation-duration) * 1.33);
+      }
+    `;
+
+    const choiceWrapperDefault = `
+      .choice-wrapper {
+        transition: transform ease-in-out var(--animation-duration),
+                    opacity ease-in-out var(--animation-duration),
+                    visibility var(--animation-duration);
+      }
+    `;
+
+    // TODO Remove Me
+    const choiceDefault = `
+      .choice {
+        transition: transform ease-in-out calc(var(--animation-duration) * 0.33);
+      }
+    `;
+
+    const choiceHouseDelay = `
+      .choice-1.house ,
+      .choice-2.house ,
+      .choice-3.house {
+        transition: transform ease-in-out var(--animation-duration)
+                    var(--animation-duration),
+                    opacity ease-in-out var(--animation-duration)
+                    var(--animation-duration);
+      }
+    `;
+
+    const bgTop = `
+      .bg-top {
+        transition: transform ease-in-out 100ms;
+      }
+    `;
+
+    const bgShadow = `
+      .bg-shadow {
+        transition: transform ease-in-out 100ms;
+      }
+    `;
+
+    const transitionModalClosed = `
+      .modal-wrapper {
+        transition: visibility 0ms var(--animation-duration),
+                    opacity ease-in-out var(--animation-duration);
+      }
+    `;
+
+    const transitionModalOpen = `
+      .modal-wrapper.modal-show {
+        transition: opacity ease-in-out var(--animation-duration),
+                    visibility;
+      }
+    `
+
+    styleSheet.insertRule(choiceDefault, styleSheet.rules.length);
+    styleSheet.insertRule(gameSelectionBgVisible, styleSheet.rules.length);
+    styleSheet.insertRule(choiceWrapperDefault, styleSheet.rules.length);
+    styleSheet.insertRule(choiceHouseDelay, styleSheet.rules.length);
+    styleSheet.insertRule(bgTop, styleSheet.rules.length);
+    styleSheet.insertRule(bgShadow, styleSheet.rules.length);
+    styleSheet.insertRule(transitionModalOpen, styleSheet.rules.length);
+    styleSheet.insertRule(transitionModalClosed, styleSheet.rules.length);
+
+    showInitialState();
+  }
+});
+
+// Modal Event Hnadling
+
+btnOpenRules.addEventListener('click', () => modalRulesWrapper.classList.add('modal-show'));
+btnCloseRules.addEventListener('click', () => modalRulesWrapper.classList.remove('modal-show'));
+modalRulesWrapper.addEventListener('click', () => {
+  modalRulesWrapper.classList.remove('modal-show');
+});
+modalRulesContent.addEventListener('click', e => {
+  e.stopPropagation();
+});
+
+// Control Game State Animation
+
+const hideInitialBg = () => {
+  bgChoices.classList.add('hidden');
+};
+
+const showInitialBg = () => {
+  bgChoices.classList.remove('hidden');
+};
+
+const hideInitialChoices = () => {
+  btnChoices.forEach(choice => {
+    choice.classList.remove('initial');
+    choice.classList.add('hidden');
+  });
+};
+
+const showInitialChoices = () => {
+  btnChoices.forEach(choice => {
+    choice.classList.remove('hidden');
+    choice.classList.add('initial');
+  });
+};
+
+btnChoices.forEach(choice => {
+  choice.addEventListener('transitionend', showInitialBg);
+});
+
+const hideInitialState = () => {
+  btnChoices.forEach(choice => {
+    choice.removeEventListener('transitionend', showInitialBg);
+    choice.addEventListener('transitionend', markAnimationEnd);
+  });
+  bgChoices.addEventListener('transitionend', hideInitialChoices);
+  hideInitialBg();
+}
+
+const showInitialState = () => {
+  bgChoices.removeEventListener('transitionend', hideInitialChoices);
+  btnChoices.forEach(choice => {
+    choice.addEventListener('transitionend', showInitialBg);
+    choice.removeEventListener('transitionend', markAnimationEnd);
+  });
+  showInitialChoices();
+}
+
+// Choice Event Hnadling
+
+btnChoices.forEach(choice => {
+  choice.addEventListener('click', () => {
+
+    const choiceHouse = computerPlay();
+    const choicePlayer = choice.dataset.choice.toLowerCase();
+
+    const tagPlayer = document
+          .querySelector(`.choice-wrapper[data-choice="${choicePlayer}"]`)
+          .cloneNode(true);
+    const tagHouse = document
+          .querySelector(`.choice-wrapper[data-choice="${choiceHouse}"]`)
+          .cloneNode(true);
+
+    tagHouse.classList.add('hidden');
+    tagPlayer.classList.add('hidden');
+
+    hideInitialState();
+
+    containerGame.appendChild(tagHouse);
+    containerGame.appendChild(tagPlayer);
+
+    window.addEventListener('animation_end', () => {
+      tagHouse.classList.remove('hidden', 'initial');
+      tagHouse.classList.add('house');
+      tagPlayer.classList.remove('hidden', 'initial');
+      tagPlayer.classList.add('chosen');
+    });
+
+    if (choiceHouse === choicePlayer) {
+      console.log(`It's a Draw! Player Chose ${choicePlayer}, Computer Chose ${choiceHouse}`);
+    } else if (playRound(choicePlayer, choiceHouse)) {
+      console.log(`Player Win! Player Chose ${choicePlayer}, Computer Chose ${choiceHouse}`);
+    } else {
+      console.log(`Player Lose! Player Chose ${choicePlayer}, Computer Chose ${choiceHouse}`);
+    }
+
+  });
+});
+
+
+
+
+
+
+
+
+
+
 function computerPlay() {
   let choice = Math.floor(Math.random() * 3 + 1);
   switch (choice) {
     case 1:
-      return "Rock";
+      return "rock";
     case 2:
-      return "Paper";
+      return "paper";
     case 3:
-      return "Scissors";
+      return "scissors";
   }
 }
 
 function playRound(playerSelection, computerSelection) {
   let playerWin = false;
-
   switch (playerSelection) {
-    case "Rock":
-      if (computerSelection === "Scissors") {
+    case "rock":
+      if (computerSelection === "scissors") {
         playerWin = true;
       }
       break;
-    case "Paper":
-      if (computerSelection === "Rock") {
+    case "paper":
+      if (computerSelection === "rock") {
         playerWin = true;
       }
       break;
-    case "Scissors":
-      if (computerSelection === "Paper") {
+    case "scissors":
+      if (computerSelection === "paper") {
         playerWin = true;
       }
       break;
   }
-
   return playerWin;
 }
 
-function capitalize(word) {
-  return word.replace(/^[a-z]/, (c) => c.toUpperCase());
+// Custom Event Handler for animationEnd
+
+const animationEnd = new CustomEvent('animation_end');
+
+const markAnimationEnd = () => {
+  window.dispatchEvent(animationEnd);
 }
 
-function game() {
-  let round = 0;
-  let playerScore = 0;
+// function capitalize(word) {
+//   return word.replace(/^[a-z]/, (c) => c.toUpperCase());
+// }
 
-  while (1) {
-    const computerSelection = computerPlay();
-    let playerSelection = prompt("Choose Your Weapon! Rock | Paper | Scissors");
-    let playerWin = false;
+// function game() {
+//   let round = 0;
+//   let playerScore = 0;
 
-    while (
-      playerSelection.toLowerCase() !== "rock" &&
-      playerSelection.toLowerCase() !== "paper" &&
-      playerSelection.toLowerCase() !== "scissors"
-    ) {
-      playerSelection = prompt(
-        "Invalid Choice!\nChoose Your Weapon! Rock | Paper | Scissors"
-      );
-    }
+//   while (1) {
+//     const computerSelection = computerPlay();
+//     let playerSelection = prompt("Choose Your Weapon! Rock | Paper | Scissors");
+//     let playerWin = false;
 
-    playerSelection = capitalize(playerSelection.toLowerCase());
+//     while (
+//       playerSelection.toLowerCase() !== "rock" &&
+//       playerSelection.toLowerCase() !== "paper" &&
+//       playerSelection.toLowerCase() !== "scissors"
+//     ) {
+//       playerSelection = prompt(
+//         "Invalid Choice!\nChoose Your Weapon! Rock | Paper | Scissors"
+//       );
+//     }
 
-    playerWin = playRound(playerSelection, computerSelection);
+//     playerSelection = capitalize(playerSelection.toLowerCase());
 
-    if (playerWin) {
-      console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
-      alert(`You Win! ${playerSelection} beats ${computerSelection}`);
-      playerScore++;
-    } else if (playerSelection === computerSelection) {
-      console.log(`Draw! Both players chose ${playerSelection}`);
-      alert(`Draw! Both players chose ${playerSelection}`);
-    } else {
-      console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-      alert(`You Lose! ${computerSelection} beats ${playerSelection}`);
-    }
+//     playerWin = playRound(playerSelection, computerSelection);
 
-    round++;
-  }
+//     if (playerWin) {
+//       console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+//       alert(`You Win! ${playerSelection} beats ${computerSelection}`);
+//       playerScore++;
+//     } else if (playerSelection === computerSelection) {
+//       console.log(`Draw! Both players chose ${playerSelection}`);
+//       alert(`Draw! Both players chose ${playerSelection}`);
+//     } else {
+//       console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+//       alert(`You Lose! ${computerSelection} beats ${playerSelection}`);
+//     }
 
-  if (playerScore > 2) {
-    console.log(`You Win The Game!!\nYou Won ${playerScore} rounds out of 5!`);
-    alert(`You Win The Game!!\nYou Won ${playerScore} rounds out of 5!`);
-  } else {
-    console.log(
-      `You Lose The Game!!\nYou Lost ${5 - playerScore} rounds out of 5!`
-    );
-    alert(`You Lose The Game!!\nYou Lost ${5 - playerScore} rounds out of 5!`);
-  }
-}
+//     round++;
+//   }
 
-game();
+//   if (playerScore > 2) {
+//     console.log(`You Win The Game!!\nYou Won ${playerScore} rounds out of 5!`);
+//     alert(`You Win The Game!!\nYou Won ${playerScore} rounds out of 5!`);
+//   } else {
+//     console.log(
+//       `You Lose The Game!!\nYou Lost ${5 - playerScore} rounds out of 5!`
+//     );
+//     alert(`You Lose The Game!!\nYou Lost ${5 - playerScore} rounds out of 5!`);
+//   }
+// }
+
+// game();
+
+// TEMP FUNCTIONS
+
+const tempRemoveInitial = () => {
+  btnChoices.forEach(choice => {
+    choice.classList.remove('initial');
+  });
+};
+
+const tempRemoveGame = () => {
+  document.querySelectorAll('.chosen').forEach(one => {
+    one.parentElement.removeChild(one);
+  });
+  document.querySelectorAll('.house').forEach(two => {
+    two.parentElement.removeChild(two);
+  });
+};
