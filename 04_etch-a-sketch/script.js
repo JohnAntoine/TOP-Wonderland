fed71d3e2ff1006c4f020ae463d95f0ad3cb5872
John Antoine <john.antoine.g@gmail.com>
2021-11-24 01:49:09 +0200

    EAS: Basic Functionality complete

--- 04_etch-a-sketch/script.js
+++ 04_etch-a-sketch/script.js
@@ -6,27 +6,60 @@ import { Color } from './color.js';
 
 const body = document.querySelector('body');
 const changeMode = document.querySelector('button');
+const colorPicker = document.querySelector('#color-picker');
+const cellCountLabel = document.querySelector('label span');
+const cellCount = document.querySelector('#cell-count');
 
 
-// Mode and Palette
+// Mouse state, Mode and Palette
 
+let mouseDown = false;
 let mode = 1;
 let solidColor = new Color('rgb(0, 0, 0)');
 let currentColor = new Color('rgb(0, 0, 0)');
 let backgroundColor = new Color('rgb(230, 230, 230)');
 
 
+// get cell count from slider
+
+cellCount.addEventListener('input', (e) => {
+  cellCountLabel.textContent = e.target.value;
+});
+cellCount.addEventListener('change', (e) => {
+  body.appendChild(createGrid(e.target.value));
+});
+
+
+// get color from color picker
+colorPicker.addEventListener('change', (e) => {
+  const color = e.target.value;
+  const r = parseInt(color.slice(1,3), 16);
+  const g = parseInt(color.slice(3,5), 16);
+  const b = parseInt(color.slice(5), 16);
+  solidColor = new Color(`rgb(${r}, ${g}, ${b})`);
+});
+
+
 // Creating and Shading cells
 
 const shadeCell = (e) => {
-  detectMode(e);
-  e.target.style.backgroundColor = currentColor.rgb();
+  if (mouseDown) {
+    detectMode(e);
+    e.target.style.backgroundColor = currentColor.rgb();
+  }
 };
 
 
-const createGrid = count => {
+function createGrid (count) {
   const sketchContainer = document.createElement('div');
   sketchContainer.classList.add('sketch', 'container');
+
+  const oldContainer = document.querySelectorAll('.sketch.container');
+  if (oldContainer.length > 0) {
+    oldContainer.forEach(container => {
+      container.parentElement.removeChild(container);
+    });
+  }
 
   for (let i = 0; i < count * count; i++) {
     const cell = document.createElement('div');
@@ -34,7 +67,12 @@ const createGrid = count => {
     cell.style.width = `calc(100% / ${count})`;
     cell.style.height = `calc(100% / ${count})`;
     cell.style.backgroundColor = backgroundColor.rgb();
-    cell.addEventListener('mouseenter', shadeCell);
+    cell.addEventListener('mousedown', (e) => {
+      mouseDown = true;
+      shadeCell(e);
+    });
+    cell.addEventListener('mousemove', shadeCell);
+    cell.addEventListener('mouseup', () => mouseDown = false);
     sketchContainer.appendChild(cell);
   }
 
@@ -47,16 +85,16 @@ changeMode.addEventListener('click', () => {
   mode++
   switch (mode) {
     case 1:
-      document.querySelector('span').textContent = 'Solid Color';
+      document.querySelector('.mode').textContent = 'Solid Color';
       break;
     case 2:
-      document.querySelector('span').textContent = 'Rainbow';
+      document.querySelector('.mode').textContent = 'Rainbow';
       break;
     case 3:
-      document.querySelector('span').textContent = 'Burn';
+      document.querySelector('.mode').textContent = 'Darken';
       break;
     case 4:
-      document.querySelector('span').textContent = 'Dodge';
+      document.querySelector('.mode').textContent = 'Lighten';
       break;
   }
 });
@@ -76,6 +114,7 @@ function detectMode(e) {
       break;
     case 3: // Burn
       bgColor = window.getComputedStyle( e.target).getPropertyValue('background-color');
+      currentColor = new Color(bgColor);
       bgColor = new Color(bgColor);
       if (bgColor.v * 100 <= 10) break;
       bgColor.changeHSV({v: (bgColor.v * 100) - 10});
@@ -83,9 +122,13 @@ function detectMode(e) {
       break;
     case 4: // Dodge
       bgColor = window.getComputedStyle( e.target).getPropertyValue('background-color');
+      currentColor = new Color(bgColor);
       bgColor = new Color(bgColor);
-      if (bgColor.v * 100 >= 90) break;
-      bgColor.changeHSV({v: (bgColor.v * 100) + 10});
+      if (bgColor.v * 100 >= 95) {
+        bgColor.changeHSV({v: 100});
+        break;
+      }
+      bgColor.changeHSV({v: (bgColor.v * 100) + 5});
       currentColor = bgColor;
       break;
   }
