5c9a19d3707b9083a4da961c4aef91e41c662da6
John Antoine <john.antoine.g@gmail.com>
2022-01-17 01:43:14 +0200

    LIB: Javascript Mock Data

--- 06_library/script.js
+++ 06_library/script.js
@@ -98,3 +98,33 @@ form.addEventListener('submit', (e) => {
 });
 
 
+////Mock Data
+// const book1 = {
+//   title: "The Hobbit",
+//   author: "J.R.R. Tolkien",
+//   pages: "310",
+//   dsc: "The Hobbit, or There and Back Again is a children's fantasy novel by English author J. R. R. Tolkien. It was published in 1937 to wide critical acclaim, being nominated for the Carnegie Medal and awarded a prize from the New York Herald Tribune for best juvenile fiction. The book remains popular and is recognized as a classic in children's literature.",
+//   read: false
+// }
+
+// const book2 = {
+//   title: "The Silmarillion",
+//   author: "J.R.R. Tolkien",
+//   pages: "365",
+//   dsc: "The Silmarillion (Quenya: [silmaˈrilliɔn]) is a collection of mythopoeic stories by the English writer J. R. R. Tolkien, edited and published posthumously by his son Christopher Tolkien in 1977 with assistance from the fantasy author Guy Gavriel Kay.[T 1] The Silmarillion tells of Eä, a fictional universe that includes the Blessed Realm of Valinor, the once-great region of Beleriand, the sunken island of Númenor, and the continent of Middle-earth, where Tolkien's most popular works—The Hobbit and The Lord of the Rings—take place.",
+//   read: true
+// }
+
+// const book3 = {
+//   title: "Harry Potter and the Philosopher's Stone",
+//   author: "J. K. Rowling",
+//   pages: "223",
+//   dsc: "Harry Potter and the Philosopher's Stone is a fantasy novel written by British author J. K. Rowling. The first novel in the Harry Potter series and Rowling's debut novel, it follows Harry Potter, a young wizard who discovers his magical heritage on his eleventh birthday, when he receives a letter of acceptance to Hogwarts School of Witchcraft and Wizardry. Harry makes close friends and a few enemies during his first year at the school, and with the help of his friends, he faces an attempted comeback by the dark wizard Lord Voldemort, who killed Harry's parents, but failed to kill Harry when he was just 15 months old",
+//   read: true
+// }
+
+// addBookToLibrary(book1);
+// addBookToLibrary(book2);
+// addBookToLibrary(book3);
+
+// addLibraryToUI();
